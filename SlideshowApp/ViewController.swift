b44d89f03512a287583a015f8980e26fb25264bc
山口奈々 <7stars.0296@gmail.com>
2018-04-08 20:47:27 +0900

    更新用

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,15 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     let imageArray = ["dance_yorokobi_mai_man","dance_yorokobi_mai_woman","money_10000"]
     var index = 0
     var timer: Timer?
     
     @IBAction func playAction(_ sender: UIButton) {
-        timer = Timer.scheduledTimer()//
+        timer = Timer.scheduledTimer(timeInterval:2.0,
+                                     target:self,
+                                     selector: #selector(onTimer(timer:)),
+                                     userInfo:nil,
+                                     repeats:true)
                   }
-    
+    else {
+    timer?.invalidate()
+    timer = nil
+    }
+    }
+
     @IBAction func goAction(_ sender: UIButton) {
         index += 1
         let  image = UIImage(named :imageArray[index] )
@@ -33,11 +46,10 @@ class ViewController: UIViewController {
 
  func onTimer(timer: Timer) {
     @objc func onTimer(time: Timer) {
-        
-    }print("on Timer")
+        print("on Timer")
 }
-weak var imageView: UIImageView!
-override func viewDidLoad() {
+
+    overrride func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
